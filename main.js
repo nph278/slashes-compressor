778b10f254653e0f51f630335cf53f9ccea17c2a
nph278 <69152943+nph278@users.noreply.github.com>
2020-08-30 09:13:55 -0400

    Big improvements

--- main.js
+++ main.js
@@ -2,42 +2,34 @@
 "use strict";
 
 function compress(str) {
-    if (/[^a-zA-Z0-9 \n]/.test(str)){
-        return "Please only use a-z + A-Z + 0-9 + space + newline";
-    } else {
-        if (/[a-z]/.test(str)){
-            if (/[A-Z]/.test(str)){
-                var symbols = "!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-                var alpha = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM 1234567890\n!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-            } else {
-                var alpha = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM 1234567890\n!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-                var symbols = "QWERTYUIOPASDFGHJKLZXCVBNM!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-            }
-        } else {
-            var symbols = "qwertyuiopasdfghjklzxcvbnm!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-            var alpha = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm 1234567890\n!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
-        }
-        var counter = 0;
-        for (var i = 0; i < alpha.length; i++) {
-            for (var j = 0; j < alpha.length; j++) {
-                if (counter<symbols.length&&("/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter])).length<str.length) {
-                    str = "/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter]);
-                    counter++;
-                }
-            }
-        }
-        for (var i = 0; i < alpha.length; i++) {
-            for (var j = 0; j < alpha.length; j++) {
-                if (counter<symbols.length&&("/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter])).length==str.length) {
-                    str = "/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter]);
-                    counter++;
-                }
-            }
-        }
-        if (("/~/\\/\\//"+str.split("//").join("~")).length<str.length) {
-            str = "/~/\\/\\//"+str.split("//").join("~");
-            counter++;
-        }
-        return str;
+    var setstr = Array.from(new Set(str)).join("");
+    var symbols = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789 \n!\"#$%&'()*+,-.:;<=>?@[]^_`{|}";
+    for (var i = 0; i < setstr.length; i++) {
+        var a = symbols.indexOf(setstr[i]);
+        symbols = symbols.slice(0,a)+symbols.slice(a+1,symbols.length);
     }
+    alert(symbols);
+    var alpha = setstr + symbols;
+    var counter = 0;
+    for (var i = 0; i < alpha.length; i++) {
+        for (var j = 0; j < alpha.length; j++) {
+            if (counter<symbols.length&&("/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter])).length<str.length) {
+                str = "/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter]);
+                counter++;
+            }
+        }
+    }
+    for (var i = 0; i < alpha.length; i++) {
+        for (var j = 0; j < alpha.length; j++) {
+            if (counter<symbols.length&&("/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter])).length==str.length) {
+                str = "/"+symbols[counter]+"/"+alpha[i]+alpha[j]+"/"+str.split(alpha[i]+alpha[j]).join(symbols[counter]);
+                    counter++;
+            }
+        }
+    }
+    if (("/~/\\/\\//"+str.split("//").join("~")).length<str.length) {
+        str = "/~/\\/\\//"+str.split("//").join("~");
+        counter++;
+    }
+    return str;
 }
